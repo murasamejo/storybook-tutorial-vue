99eca3e25c377f9eaa8ea7725d2303b2683dbbee
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-09-05 22:16:37 +0900

    feat: 🎸 次のチャプター（"Screens"）の準備

---
+++ src/components/PureInboxScreen.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <!-- TODO: https://storybook.js.org/tutorials/intro-to-storybook/vue/en/screen/ -->
+  </div>
+</template>
+
+<script>
+// TODO: https://storybook.js.org/tutorials/intro-to-storybook/vue/en/screen/
+</script>
